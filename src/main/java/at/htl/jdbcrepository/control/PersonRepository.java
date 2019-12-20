aa9c1f93e4d41accca162064bd3d7b5ea2c3a18b
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-12-20 12:35:30 +0100

    Person Repository Constants

---
+++ src/main/java/at/htl/jdbcrepository/control/PersonRepository.java
@@ -0,0 +1,7 @@
+package at.htl.jdbcrepository.control;
+
+public class PersonRepository {
+    private static final String url = "jdbc:derby://localhost:1527/db;create=true";
+    private static final String username = "app";
+    private static final String password = "app";
+}

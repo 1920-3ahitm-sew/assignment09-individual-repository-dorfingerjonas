15d6e53a7e96ad3c7b49767421d59c6acf43efc8
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-12-21 18:56:19 +0100

    very simple createTable- and insertRecord- methods added

--- src/main/java/at/htl/jdbcrepository/control/PersonRepository.java
+++ src/main/java/at/htl/jdbcrepository/control/PersonRepository.java
@@ -1,7 +1,60 @@
 package at.htl.jdbcrepository.control;
 
-public class PersonRepository {
-    private static final String url = "jdbc:derby://localhost:1527/db;create=true";
-    private static final String username = "app";
-    private static final String password = "app";
+import at.htl.jdbcrepository.entity.Person;
+import at.htl.jdbcrepository.entity.Repository;
+
+import java.sql.*;
+
+public class PersonRepository implements Repository {
+    public static final String url = "jdbc:derby://localhost:1527/db;create=true";
+    public static final String username = "app";
+    public static final String password = "app";
+
+    public void createTable() {
+        try (Connection conn = DriverManager.getConnection( // <1>
+                url,
+                username,
+                password)) {
+            try (Statement stmt = conn.createStatement()) {  // <1>
+                String sql = "CREATE TABLE person (" +
+                        "id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY," +
+                        "name VARCHAR(255)," +
+                        "city VARCHAR(255)," +
+                        "house VARCHAR(255)" +
+                        ")";
+                stmt.executeUpdate(sql);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public Person save(Person p) {
+        try (Connection conn = DriverManager.getConnection(
+                url,
+                username,
+                password)) {
+
+            String sql = "INSERT INTO person (NAME, CITY, HOUSE) " +
+                         "VALUES (?,?,?)";
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+                pstmt.setString(1,p.getName());
+                pstmt.setString(2,p.getCity());
+                pstmt.setString(3,p.getHouse());
+                int rowAffected = pstmt.executeUpdate();
+                System.out.println("Zeilen eingefügt: " + rowAffected);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
 }
